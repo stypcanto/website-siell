9bd6ab96bdb6ee67a6b189581aef64dd936fe69f
Styp Canto <styp611@outlook.com>
2023-08-14 01:03:17 -0500

    Arreglando estilos para el menu hamburguer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const switchInput = document.getElementById('switch');
 const menuHamButton = document.querySelector('.hamburger-menu');  //Icono de las tres rayitas
 const mobileMenu = document.querySelector('.mobile-menu'); //Clase mobile-menu
-
+const content = document.querySelector('.content');
 
 // Agregar event listeners a los elementos seleccionados
 menuHamButton.addEventListener('click', toggleMenu); // Corrección de la variable a menuHamButton
@@ -10,7 +10,7 @@ menuHamButton.addEventListener('click', toggleMenu); // Corrección de la variab
 
 
 switchInput.addEventListener('change', function () {
-  
+
   if (this.checked) {
     document.body.classList.add('dark-mode'); // Agrega la clase para modo oscuro
   } else {
@@ -21,7 +21,13 @@ switchInput.addEventListener('change', function () {
 
 
 function toggleMenu() { // Cambio de nombre de función para ser coherente
-    mobileMenu.classList.toggle('active'); // Cambia la clase para mostrar u ocultar el menú
+  mobileMenu.classList.toggle('active'); // Cambia la clase para mostrar u ocultar el menú
+  // Si el menú está activo, oculta el contenido; de lo contrario, muestra el contenido
+  if (mobileMenu.classList.contains('active')) {
+    content.style.display = 'none';
+  } else {
+    content.style.display = 'block'; // O cualquier otro valor de display que corresponda
+  }
 }
 
 
